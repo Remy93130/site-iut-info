87e006f3530f6424643f547807a6958f3d1dcd88
Remy93130 <remy93130@gmail.com>
2019-02-08 20:39:55 +0100

    Javascript filter
    TODO: Animation

--- public/js/offer_search.js
+++ public/js/offer_search.js
@@ -1,20 +1,27 @@
-function searchElement() {
-	
-	return;
+"use strict"
+function alterCheckbox(checkbox) {
+    let typeOffer = $(checkbox).attr('id');
+    if ($(checkbox).is(':checked')) {
+        $(`.${typeOffer}`).css('display', 'block');
+    } else {
+        $(`.${typeOffer}`).css('display', 'none');
+    }
+}
+
+function getCheckedBox() {
+    let stage = $('#stage').is(':checked')? 1 : 0;
+    let alternance = $('#alternance').is(':checked')? 1 : 0;
+    if (stage && alternance) return "emploi";
+    return (stage)? "stage" : "alternance";
 }
 
 $("#input_search").keyup(e => {
-	let contentSearch = $("#input_search").val();
-	if (contentSearch === "") {
-		$(".emploi").css("display", "none");
-	} else {
-		$(".emploi").css("display", "block");
-		searchElement();
-	}
-});
-
-$("#stage").change(e => {
-    if (this.checked) {
-        //Do stuff
-    }
+    let value = $("#input_search").val();
+    $(".emploi").each((index, element) => {
+        if (element.innerHTML.indexOf(value) !== -1 && $(element).hasClass(getCheckedBox())) {
+            $(element).css('display', "block");
+        } else {
+            $(element).css('display', "none");
+        }
+    });
 });
