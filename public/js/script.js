3213fea28d2362e175cb4fc10a888601b377fbc9
LinKinoMalandrin <39130976+LinKinoMalandrin@users.noreply.github.com>
2019-02-27 15:44:17 +0100

    added scroll
    Du coup je pense qu'on a fini ^^

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+window.onscroll = function() { scrollFunction() };
+
 function openNavbar(button, Navbar){
 	document.getElementById(Navbar).classList.add('open');
 	button.onclick = function() { closeNavbar(button, Navbar); }
@@ -14,3 +16,15 @@ function useModal(modalID){
 	var modal = document.getElementById(modalID);
 	modal.style.display = 'block';
 }
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementById("UPARROW").classList.remove("hidden");
+  } else {
+    document.getElementById("UPARROW").classList.add("hidden");
+  }
+}
+function toTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
